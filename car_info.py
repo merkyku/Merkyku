e935990c36f6376f0fe0569cd1dea8ecf7a1f308
Feodor <mer.kyku81@mail.ru>
2023-04-25 13:59:04 +0300

    One car class

--- car_info.py
+++ car_info.py
@@ -8,19 +8,18 @@ class Coordinate:
 class Car:
     def __init__(self, x, y):
         self.health = 100
-        self.speed = 3
         self.pos = Coordinate(x, y)
         self.is_alive = True
         self.sprite = 'O'
 
     def move(self, move_choice):
         if move_choice == 1:
-            self.pos.x += 30
+            self.pos.x += 15
         elif move_choice == 2:
-            self.pos.x -= 30
+            self.pos.x -= 15
 
     def take_damage(self, damage):
         self.health -= damage
-        if self.health <=0:
+        if self.health <= 0:
             self.is_alive = False
 
