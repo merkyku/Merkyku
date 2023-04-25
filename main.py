87295155af421c268a4adcfcb39b2dcfbf9bd7f3
Feodor <mer.kyku81@mail.ru>
2023-04-25 13:30:47 +0300

    first screen

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import pygame
 def main():
-    pass
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+    pygame.display.set_caption("brick_track")
+    clock = pygame.time.Clock()
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        clock.tick(60)
 
 
 
