ef80c9138679fd4fd499b96cef7d0677c9427392
Feodor <mer.kyku81@mail.ru>
2023-05-29 16:37:01 +0300

    pic+damage is complite

--- gameplay.py
+++ gameplay.py
@@ -1,9 +1,10 @@
 import random
 from brick_info import *
+from car_info import *
 bricks = []
 
 delta_time = 0
-cooldown = 15
+cooldown = 20
 
 def initial_game(field):
     for y in range(len(field)):
@@ -12,21 +13,24 @@ def initial_game(field):
     return field
 
 
-def brick_fallen(field):
+def brick_fallen(field, car):
     global bricks, delta_time, cooldown
     k=0
-    dead_list=[]
+    dead_list = []
     if delta_time > cooldown:
         while k < 4:
-            if random.randint(1, 10) == 1:
+            if random.randint(1, 20) == 1:
                 x = random.randint(1, len(field)-2)
-                brick = Brick(x, 0)
+                brick = Brick(x, -1)
                 bricks.append(brick)
-                k+=1
+                k += 1
         for brick in bricks:
             field[brick.pos.x][brick.pos.y] = ' '
             if brick.pos.y >= len(field)-1:
                 bricks.remove(brick)
+            elif field[brick.pos.x][brick.pos.y+1] == 'car':
+                bricks.remove(brick)
+                car.take_damage(10)
             else:
                 brick.pos.y += 1
                 field[brick.pos.x][brick.pos.y] = 'b'
--- main.py
+++ main.py
@@ -8,13 +8,27 @@ def render_pygame(field, screen):
     for x in range(0,len(field)):
         for y in range(0,len(field)):
             if field[x][y] == 'b':
-                pygame.draw.rect(screen, (255, 0, 0), (x*scale, y*scale, scale, scale))
+                pygame.draw.rect(screen, (34, 177, 77), (x*scale+19, y*scale+1, 4, 4))
+                pygame.draw.rect(screen, (34, 177, 77), (x * scale+15, y * scale+5, 4*3, 4))
+                pygame.draw.rect(screen, (34, 177, 77), (x * scale + 11, y * scale + 9, 4*5, 4))
+                pygame.draw.rect(screen, (34, 177, 77), (x * scale + 7, y * scale + 13, 4*7, 4))
+                pygame.draw.rect(screen, (34, 177, 77), (x * scale + 3, y * scale + 17, 4*9, 4))
+                pygame.draw.rect(screen, (34, 177, 77), (x * scale, y * scale + 21, 4*10, 4))
+                pygame.draw.rect(screen, (120, 63, 23), (x * scale + 17, y * scale + 25, 7, 15))
+
             if field[x][y] == 'car':
-                pygame.draw.rect(screen, (0, 255, 0), (x*scale, y*scale, scale, scale))
+                pygame.draw.rect(screen, (47, 54, 153), (x*scale+10, y*scale+6,4,8))
+                pygame.draw.rect(screen, (47, 54, 153), (x * scale + 27, y * scale + 6, 4, 8))
+                pygame.draw.rect(screen, (47, 54, 153), (x * scale + 16, y * scale + 9, 8, 2))
+                pygame.draw.rect(screen, (47, 54, 153), (x * scale + 15, y * scale + 11, 10, 3))
+                pygame.draw.rect(screen, (47, 54, 153), (x * scale + 15, y * scale + 14, 10, 20))
+                pygame.draw.rect(screen, (47, 54, 153), (x * scale + 10, y * scale + 30, 4, 8))
+                pygame.draw.rect(screen, (47, 54, 153), (x * scale + 27, y * scale +30, 4, 8))
+                pygame.draw.rect(screen, (255, 255, 255), (x * scale + 18, y * scale + 15, 4, 13))
+
 
 
 def main():
-
     field = [[' '] * 20 for i in range(20)]
     field = gameplay.initial_game(field)
     x = 10
@@ -24,19 +38,21 @@ def main():
 
     pygame.init()
     pygame.font.init()
-    screen = pygame.display.set_mode((800, 800))
+    screen = pygame.display.set_mode((1000, 800))
     pygame.display.set_caption("brick_track")
     clock = pygame.time.Clock()
     is_running = True
     main_font = pygame.font.Font(None, 24)
-    text1 = main_font.render('Добро пожаловать!', True, (255, 255, 255))
     while is_running:
-        field = gameplay.brick_fallen(field)
+        field = gameplay.brick_fallen(field, car)
+        text1 = main_font.render(f'ваше здоровье {car.health}%', True, (255, 255, 255))
+        if not car.is_alive:
+            is_running = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
             if event.type == pygame.KEYDOWN:
-                text1 = main_font.render(f'ваше здоровье {car.health}%', True, (255, 255, 255))
+
                 if event.key == pygame.K_LEFT:
                     if field[car.pos.x-1][y] != 'b':
                         field[car.pos.x][y] = ' '
@@ -44,8 +60,6 @@ def main():
                         field[car.pos.x][y] = car.sprite
                     else:
                         car.take_damage(10)
-                        if not car.is_alive:
-                            is_running = False
                 if event.key == pygame.K_RIGHT:
                     if field[car.pos.x+1][y] != 'b':
                         field[car.pos.x][y] = ' '
@@ -53,10 +67,9 @@ def main():
                         field[car.pos.x][y] = car.sprite
                     else:
                         car.take_damage(10)
-                        if not car.is_alive:
-                            is_running = False
+
         screen.fill((0, 0, 0))
-        screen.blit(text1, (50, 780))
+        screen.blit(text1, (820, 10))
         render_pygame(field, screen)
         pygame.display.flip()
 
